1f88c7489cd1be7ef8d38c82b5880f5766705f37
GeorgeMakhlouf05 <gmakhlouf@unab.edu.co>
2026-02-27 09:30:54 -0500

    Primera Pantalla

--- app/src/main/java/com/example/quizandroidgeorgemakhlouf/MainActivity.kt
+++ app/src/main/java/com/example/quizandroidgeorgemakhlouf/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quizandroidgeorgemakhlouf.ui.theme.QuizAndroidGeorgeMakhloufTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +38,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuizAndroidGeorgeMakhloufTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Pantalla1()
+
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    QuizAndroidGeorgeMakhloufTheme {
-        Greeting("Android")
+fun Pantalla1() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Encabezado",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+
+
+
+        )
+
+
+
+
+        Spacer(modifier = Modifier.height(400.dp))
+
+        Text(
+            text = "Item1             Item2              Item3",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+        Spacer(modifier = Modifier.height(350.dp))
+
+        Text(
+            text = "Pie de página",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+
     }
 }
+
+
+
+
+
+
+
